968b2720a27764ac41c2d39010112b7cc09aa746
Aline K. Miyazaki <alinekm03@gmail.com>
2014-05-14 18:19:12 -0300

    Initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CharlieAndDogs::Application.config.secret_key_base = '83d50d404b26528ef372356b8fbad784a239a1e6d47036b5005d9f47bfe64aaa19b725d06fb636b39b78a25fddd8dd046033428677c9faca65669ef27dc4ae77'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CharlieAndDogs::Application.config.session_store :cookie_store, key: '_charlie_and_dogs_session'
